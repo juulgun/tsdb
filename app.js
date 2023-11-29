94e25c71120cd43f2b5c76f1e3d59b3aa84516e8
Julie Gunning <juulgunning@gmail.com>
2023-11-29 11:44:18 +0100

    code werkt op azure app service

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ app.use('/data', dataRoute)
 app.use(express.static(__dirname + '/public'));
 app.use('/scripts',express.static(path.join(__dirname, 'public/scripts')));
 
-app.listen(8080)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
